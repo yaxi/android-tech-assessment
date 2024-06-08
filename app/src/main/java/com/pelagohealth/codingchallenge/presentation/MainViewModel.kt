560bc523e60d1b27e9948e90d0c9ad5e1c191ff5
Yaxi Ye <yaxi.ye@gmail.com>
2024-06-08 19:50:12 +0100

    add fetch & remove fact methods to viewmodel, add view state for main screen
    (cherry picked from commit 22edfd22a56830ec662aaf80a88417c88c2154c5)

--- app/src/main/java/com/pelagohealth/codingchallenge/presentation/MainViewModel.kt
+++ app/src/main/java/com/pelagohealth/codingchallenge/presentation/MainViewModel.kt
@@ -1,11 +1,64 @@
 package com.pelagohealth.codingchallenge.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.pelagohealth.codingchallenge.data.repository.FactRepository
+import com.pelagohealth.codingchallenge.domain.CoroutineDispatcherModule
+import com.pelagohealth.codingchallenge.domain.CoroutineDispatchers
+import com.pelagohealth.codingchallenge.domain.model.Fact
+import com.pelagohealth.codingchallenge.presentation.ui.viewstate.MainViewState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel : ViewModel() {
+class MainViewModel @Inject constructor(
+    private val repository: FactRepository,
+    private val dispatchers: CoroutineDispatchers
+): ViewModel() {
+
+    private val _viewState = MutableStateFlow(MainViewState())
+    val viewState: StateFlow<MainViewState> = _viewState.asStateFlow()
+
     fun fetchNewFact() {
-        // TODO: get a new fact from the REST API and display it to the user
+        viewModelScope.launch(dispatchers.io) {
+            setState { copy(isLoading = true) }
+            val fact = repository.getFact()
+            setState {
+                val list = ArrayDeque(_viewState.value.facts)
+                val current = _viewState.value.fact
+                list.add(current)
+                trimList(list)
+                copy(
+                    fact = fact,
+                    facts = list,
+                    isLoading = false
+                )
+            }
+        }
+    }
+
+    fun removeFact(id: String) {
+        val facts = _viewState.value.facts.toMutableList().filterNot {
+            it.id == id
+        }
+        setState { copy(facts = facts) }
+    }
+
+    private fun trimList(list: ArrayDeque<Fact>) {
+        if (list.size >= 3) {
+            for (i in 3 until list.size) {
+                list.removeLast()
+            }
+        }
+    }
+
+    private fun setState(reducer: MainViewState.() -> MainViewState) {
+        viewModelScope.launch(dispatchers.main) {
+            _viewState.value = reducer(_viewState.value)
+        }
     }
 }
---
+++ app/src/main/java/com/pelagohealth/codingchallenge/presentation/ui/viewstate/MainViewState.kt
@@ -0,0 +1,10 @@
+package com.pelagohealth.codingchallenge.presentation.ui.viewstate
+
+import com.pelagohealth.codingchallenge.domain.model.Fact
+import java.util.LinkedList
+
+data class MainViewState(
+    val fact: Fact = Fact(text = "", url = "", id = ""),
+    val facts: List<Fact> = ArrayDeque(),
+    val isLoading: Boolean = false
+)
