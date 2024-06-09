379d865c84c43070ccc5763c172a7c80b5744636
Yaxi Ye <yaxi.ye@gmail.com>
2024-06-09 12:38:41 +0100

    add dagger annotation

--- app/src/main/java/com/pelagohealth/codingchallenge/data/repository/FactRepository.kt
+++ app/src/main/java/com/pelagohealth/codingchallenge/data/repository/FactRepository.kt
@@ -1,13 +1,15 @@
 package com.pelagohealth.codingchallenge.data.repository
 
-import com.pelagohealth.codingchallenge.data.FactMapper
+import com.pelagohealth.codingchallenge.data.mapper.FactMapper
 import com.pelagohealth.codingchallenge.data.datasource.rest.FactsRestApi
 import com.pelagohealth.codingchallenge.domain.model.Fact
 import javax.inject.Inject
+import javax.inject.Singleton
 
 /**
  * Repository providing random facts.
  */
+@Singleton
 class FactRepository @Inject constructor(
     private val api: FactsRestApi,
     private val mapper: FactMapper
