27973f8fecbdf565839514d2b223599cdc78e301
Yaxi Ye <yaxi.ye@gmail.com>
2024-06-09 15:25:04 +0100

    add ConstraintLayout lib

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
 
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
