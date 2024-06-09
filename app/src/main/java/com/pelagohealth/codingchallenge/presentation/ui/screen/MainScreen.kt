cc5af350ab6002026c918411213cbb714d966ba1
Yaxi Ye <yaxi.ye@gmail.com>
2024-06-09 12:39:08 +0100

    update on main screen to use viewstate to display data

--- app/src/main/java/com/pelagohealth/codingchallenge/presentation/ui/screen/MainScreen.kt
+++ app/src/main/java/com/pelagohealth/codingchallenge/presentation/ui/screen/MainScreen.kt
@@ -3,12 +3,17 @@ package com.pelagohealth.codingchallenge.presentation.ui.screen
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.pelagohealth.codingchallenge.presentation.MainViewModel
 import com.pelagohealth.codingchallenge.ui.theme.PelagoCodingChallengeTheme
@@ -16,6 +21,7 @@ import com.pelagohealth.codingchallenge.ui.theme.PelagoCodingChallengeTheme
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
     val viewModel: MainViewModel = viewModel()
+    val state by viewModel.viewState.collectAsState()
 
     Column(
         modifier = modifier.fillMaxSize(),
@@ -23,11 +29,19 @@ fun MainScreen(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = "Hello!",
+            text = state.fact.text,
             modifier = modifier
         )
         Button(onClick = { viewModel.fetchNewFact() }) {
             Text("More facts!")
+        }
+        LazyColumn {
+            items(
+                items = state.facts,
+                key = { it.id }
+            ) {fact ->
+                Text(text = fact.text)
+            }
         }
     }
 }
